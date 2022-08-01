48ba87f8be2e809479f58a35a2a3ab4c84d7f678
Ednildo Macena <nildo.777@gmail.com>
2022-08-01 13:43:22 -0300

    First commit

---
+++ src/pages/Cardapio/Buscador/Buscador.tsx
@@ -0,0 +1,9 @@
+
+interface Props {
+    busca: string,
+    setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Buscador({ busca, setBusca }: Props) {
+    return <div></div>;
+}
